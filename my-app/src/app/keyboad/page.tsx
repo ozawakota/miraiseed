fdd9262f6dcd44e6971cb2d873a864384b3f670e
k_ozawa <kouta.ozawa@nadia.bz>
2024-10-17 17:49:36 +0900

    deploy

---
+++ my-app/src/app/keyboad/page.tsx
@@ -0,0 +1,12 @@
+import MyPage from "./myPage"
+
+export const metadata = {
+  title: "日本語入力制御ページ",
+  description: "日本語入力制御ページ",
+}
+
+const KeyBoad = () => {
+    return <MyPage/>
+}
+
+export default KeyBoad
